34255f3aa3fb7ddd3a704ae2379e6b563593dd1d
Barclay <barclayiversen@gmail.com>
2023-12-29 13:54:21 -0800

    update CI and add service I was missing

--- terraform/prod/services.tf
+++ terraform/prod/services.tf
@@ -47,3 +47,8 @@ resource "google_project_service" "iamcredentials_googleapis_com" {
   project = var.project_number
   service = "iamcredentials.googleapis.com"
 }
+
+resource "google_project_service" "cloudbuild_googleapis_com" {
+  project = var.project_number
+  service = "cloudbuild.googleapis.com"
+}
