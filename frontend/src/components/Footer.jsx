500c0b4389d8c6ba4fac6b9847aae707cb546e5d
Barclay <barclayiversen@gmail.com>
2023-12-29 16:03:24 -0800

    Footer update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,12 +5,9 @@ function Footer() {
 
   return (
     <footer className="bg-gray-100 text-black p-10 md:flex md:justify-center md:items-center text-center mt-20">
-      <div className="flex-1 mt-4 md:mt-0 md:order-2">
-        <p>© {currentYear} Spracto</p>
-      </div>
-      <div className="flex-1 md:border-r md:border-gray-400 pr-4 md:order-1">
+      <div className="flex-1 md:border-r md:border-gray-400 md:order-1 font-bold">
         <p>
-          Wanna know about upcoming shows? Connect with Spracto on Instagram{" "}
+          Wanna hear about upcoming shows? Connect with Spracto on Instagram{" "}
           <a
             href="https://www.instagram.com/spracto"
             target="_blank"
@@ -20,6 +17,9 @@ function Footer() {
             @spracto
           </a>
         </p>
+      </div>
+      <div className="flex-1 mt-4 md:mt-0 md:order-2">
+        <p>© {currentYear} Spracto</p>
       </div>
     </footer>
   );
