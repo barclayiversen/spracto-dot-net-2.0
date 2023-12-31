3a44e70abe16293f2a4c69c822f221f015977e9d
Barclay <barclayiversen@gmail.com>
2023-12-31 10:19:00 -0800

    implement loading to mitigate FOUC

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,16 +1,27 @@
-import Image from "next/image";
-
+import React, { useState, useEffect } from "react";
+import LoadingSpinner from "@/components/loading";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import BackgroundVideoSection from "@/components/backgroundVideoSection";
 import AboutSection from "@/components/about";
 import Releases from "@/components/releases";
 import PhotoCarousel from "@/components/photoCarousel";
-import { Link } from "react-scroll"; // Assuming you're using react-scroll
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loading = async () => {
+      setIsLoading(false);
+    };
+
+    loading();
+  }, []);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <main>
       <Header />
