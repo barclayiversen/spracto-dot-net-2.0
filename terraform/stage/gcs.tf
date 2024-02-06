1c88fcc00439a5ec2578cd9f12a58e7c8b93df0c
Barclay <barclayiversen@gmail.com>
2024-02-06 00:15:09 -0800

    terraform updates

--- terraform/stage/gcs.tf
+++ terraform/stage/gcs.tf
@@ -1,11 +1,11 @@
-resource "google_storage_bucket" "spracto_net_images" {
-  force_destroy            = false
-  location                 = "US-WEST1"
-  name                     = "spracto-net-images-stage"
-  project                  = var.project-id
-  public_access_prevention = "inherited"
-  storage_class            = "STANDARD"
-}
+# resource "google_storage_bucket" "spracto_net_images" {
+#   force_destroy            = false
+#   location                 = "US-WEST1"
+#   name                     = "spracto-net-images-stage"
+#   project                  = var.project-id
+#   public_access_prevention = "inherited"
+#   storage_class            = "STANDARD"
+# }
 
 resource "google_storage_bucket" "spracto_net_tfstate" {
   force_destroy            = false
--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -17,11 +17,23 @@ variable "state_bucket" {
 variable "spracto-net-backend-roles" {
   description = "List of IAM roles for backend SA"
   type        = list(string)
-  default     = ["roles/datastore.user", "roles/datastore.viewer", "roles/storage.objects.create"]  # Example roles
+  default     = ["roles/datastore.user", "roles/datastore.viewer", "roles/storage.objectCreator"]  # Example roles
 }
+
+//Anti pattern
+variable "spracto-net-backend-roles-prd" {
+  description = "List of IAM roles for backend SA"
+  type        = list(string)
+  default     = ["roles/storage.objectCreator", "roles/storage.objectUser"]  # Example roles
+}
+
+variable "prd_project_id" {
+  description = "alt prj id"
+}
+
 
 variable "spracto-net-gh-actions-roles" {
   description = "List of IAM roles for gh actions SA"
   type        = list(string)
-  default     = ["roles/appengine.deployer", "roles/appengine.appAdmin", "roles/cloudbuild.builds.editor", "roles/run.admin", "roles/iam.serviceAccountUser", "roles/storage.admin", "roles/iam.workloadIdentityUser"]  # Example roles
+  default     = ["roles/cloudbuild.builds.editor", "roles/run.admin", "roles/iam.serviceAccountUser", "roles/storage.admin", "roles/iam.workloadIdentityUser"]  # Example roles
 }
