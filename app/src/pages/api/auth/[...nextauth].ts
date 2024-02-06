41b19a0138b9937399f723d561538ddf1d5899ef
Barclay <barclayiversen@gmail.com>
2024-02-05 20:43:08 -0800

    update auth

--- app/src/pages/api/auth/[...nextauth].ts
+++ app/src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 // Ensure environment variables are defined
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+// Use a more descriptive name for the base URL environment variable
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
   throw new Error("GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET is not defined");
@@ -14,6 +16,12 @@ export default NextAuth({
     GoogleProvider({
       clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
+      // Dynamically set the callback URL based on the environment
+      authorization: {
+        params: {
+          redirect_uri: `${NEXT_PUBLIC_BASE_URL}/api/auth/callback/google`,
+        },
+      },
     }),
   ],
   callbacks: {
