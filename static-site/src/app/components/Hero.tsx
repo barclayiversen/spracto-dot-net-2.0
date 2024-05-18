51489917a79bf1db77fe4ea32c2c35e824837e8a
Barclay <barclayiversen@gmail.com>
2024-05-18 12:14:59 -0700

    static components updates

--- static-site/src/app/components/Hero.tsx
+++ static-site/src/app/components/Hero.tsx
@@ -12,6 +12,7 @@ const Hero: React.FC = () => {
         <source src="/bgvideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className="absolute inset-0 bg-black opacity-25"></div>
       <div className="relative flex items-center justify-center h-full">
         <img src="/logo-black.png" alt="My Logo" className="h-48 w-auto" />
       </div>
--- static-site/src/app/components/Show.tsx
+++ static-site/src/app/components/Show.tsx
@@ -8,10 +8,10 @@ const Show: React.FC<{
 }> = ({ src, alt, buttonText, onClick }) => {
   return (
     <div className="text-center">
-      <img src={src} alt={alt} className="mx-auto mb-4" />
+      <img src={src} alt={alt} className="mx-auto mb-4 rounded-3xl" />
       <button
         onClick={onClick}
-        className="bg-blue-500 text-white py-2 px-4 rounded"
+        className="bg-blue-500 text-white py-2 px-4 rounded-full"
       >
         {buttonText}
       </button>
